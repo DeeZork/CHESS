a6266580afdba8abcb151934fd840746c0e4ef51
DeeZork <DeeZorks@gmail.com>
2023-04-18 18:48:40 +0300

    Задача 1: ОК Задача 2: OK Задача 3: OK Задача 4: OK Задача 5: OK Задача 6: OK

---
+++ King.java
@@ -0,0 +1,24 @@
+public class King extends ChessPiece{
+    King(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        boolean can=false;
+        if (chessBoard.chekToPos(line, column, toLine, toColumn)&&
+                ((Math.abs(toLine-line)==1||Math.abs(toColumn-column)==1)))
+            can=true;
+        return can;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "K";
+    }
+}
